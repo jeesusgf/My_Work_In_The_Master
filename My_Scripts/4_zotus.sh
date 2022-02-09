990c8085645a0cc2a02c337c6338942b191654e5
Carlos <89907733+biocompt@users.noreply.github.com>
2022-02-09 18:17:46 +0100

    Update 4_zotus.sh

--- My_Scripts/4_zotus.sh
+++ My_Scripts/4_zotus.sh
@@ -27,7 +27,11 @@ mkdir Resultados
 ~/usearch -usearch_local Zotus/zotus.fa -db ~/Documents/BBD_ambiental/Especifica_16S_ambiental_v9.fasta \
 -id 0.95 -blast6out Resultados/taxonomy_recognition.outfm6 -notmatched Zotus/not_matched.fa -strand both -top_hit_only -query_cov 0.90
 
-### 6 - Blast no-matched with syntax database
+### 6 - Export data to a tsv
+
+awk '{ print $1"\t"$2 }' taxonomy_recognition.outfm6 > Tabla_especies.tsv
+
+### 7 - Blast no-matched with syntax database
 
 ~/usearch -sintax Zotus/not_matched.fa -db ~/Documents/BBD_ambiental/Sintax_RDP_16S_Version18_062020.fa \
 -strand both -tabbedout Resultados/taxonomia_predicted.tsv -sintax_cutoff 0.97
